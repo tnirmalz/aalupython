b4f984f43fbbd9f18f4c9a4941eeb5c307d745c6
Nirmal thapa <5167182-nirmalthapa@users.noreply.gitlab.com>
2020-01-20 12:44:29 +0545

    new sites added and code format

--- lazydork.py
+++ lazydork.py
@@ -2,12 +2,41 @@
 # Usage: python3 lazydork.py <target.com>
 
 import time
-import webbrowser    
+import webbrowser
 import sys
 
 target = sys.argv[1]
-osint_sites = ["stackoverflow.com","codepad.co", "scribd.com", "npmjs.com", "npm.runkit.com", "libraries.io", "ycombinator.com", "coggle.it", "papaly.com", "google.com", "trello.com", "prezi.com", "jsdelivr.net", "codepen.io", "codeshare.io", "sharecode.io", "pastebin.com", "gist.github.com", "repl.it", "productforums.google.com", "gitter.im", "bitbucket.org", "*.atlassian.net", "gitlab.com"]
+osint_sites = [
+    "stackoverflow.com",
+    "codepad.co",
+    "scribd.com",
+    "npmjs.com",
+    "npm.runkit.com",
+    "libraries.io",
+    "ycombinator.com",
+    "coggle.it",
+    "papaly.com",
+    "google.com",
+    "trello.com",
+    "prezi.com",
+    "jsdelivr.net",
+    "codepen.io",
+    "codeshare.io",
+    "sharecode.io",
+    "pastebin.com",
+    "gist.github.com",
+    "repl.it",
+    "productforums.google.com",
+    "gitter.im",
+    "bitbucket.org",
+    "*.atlassian.net",
+    "gitlab.com",
+	"speakerdeck.com",
+	"slideshare.net",
+	"linkedin.com",
+	"hub.docker.com"
+	]
 
-for onewebsite in osint_sites:
-	webbrowser.open('https://www.google.com.np/search?q=site:{}%20%22{}%22'.format(onewebsite, target))
-	time.sleep(5) # avoid captcha
+for x in osint_sites:
+    webbrowser.open('https://www.google.com.np/search?q=site:{}%20%22{}%22'.format(x, target))
+    time.sleep(6)  # avoid captcha
